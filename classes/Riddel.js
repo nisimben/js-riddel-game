d35a5fdf928b102eddcc2cff11ae0cd167466319
nisimben <nisim10b@gmail.com>
2025-12-01 14:40:09 +0200

    add uttils and fix multy-cohice

--- classes/Riddel.js
+++ classes/Riddel.js
@@ -1,18 +1,19 @@
-export class Riddel {
-    constructor(id, name, taskDescription, correctAnswer, choices) {
-        this.id = id;
-        this.name = name;
-        this.taskDescription = taskDescription;
-        this.correctAnswer = correctAnswer;
-        this.choices = choices;
-    }
+export default class Riddle {
+  constructor({ id, name, taskDescription, correctAnswer, choices = [], type}) {
+    this.id = id;
+    this.name = name;
+    this.taskDescription = taskDescription;
+    this.correctAnswer = correctAnswer;
+    this.choices = choices;
+    this.type = type;
+  }
     ask(readline){
         console.log(`\n Riddle ${this.id}: ${this.name}`);
         console.log(this.taskDescription);
         let flag = true;
         while(flag){
             let answer = readline.question('Answer: ');
-            if(answer.toLowerCase() === this.correctAnswer.toLowerCase()){
+            if(answer === this.correctAnswer){
                 console.log('Correct!');
                 flag = false;
             }
--- classes/multipleChoiceRiddle.js
+++ classes/multipleChoiceRiddle.js
@@ -1,9 +1,47 @@
-import { Riddel } from "Riddel.js";
+import  Riddle  from "./riddel.js";
 
-export class MultipleChoiceRiddle extends Riddel {
-    constructor(id, name, taskDescription, correctAnswer, choices) {
-        super(id, name, taskDescription, correctAnswer, choices);
-        this.choices = choices || [];
-        this.type = 'multiple-choice';
+export default class MultipleChoiceRiddle extends Riddle {
+  constructor(raw) {
+    super({ ...raw, type: 'multiple-choice' });
+    this.choices = raw.choices || [];
+  }
+ask(readline) {
+  console.log(`\nRiddle ${this.id}: ${this.name}`);
+  console.log(this.taskDescription);
+
+
+  this.choices.forEach((choice, index) => {
+    console.log(`${index + 1}) ${choice}`);
+  });
+
+  const correctIndex = this.choices.indexOf(this.correctAnswer);
+
+  if (correctIndex === -1) {
+    console.log(' correctAnswer not found in choices ');
+    return;
+  }
+
+  let flag = true;
+
+  while (flag) {
+    const input = readline.question('Choose a number: ');
+    const num = parseInt(input, 10);
+
+    if (Number.isNaN(num) || num < 1 || num > this.choices.length) {
+      console.log(`Invalid choice. Please enter a number between 1 and ${this.choices.length}.`);
+      continue;
     }
+
+    const chosenIndex = num - 1;
+
+    if (chosenIndex === correctIndex) {
+      console.log('Correct!');
+      flag = false;
+    } else {
+      console.log('Wrong! Please try again!');
+    }
+  }
+}
+
+
 }
